dbe73d50b8df3104c2fe8e1eeda7d9cc5bf19d67
Viviane Oliveira <oviviane772@gmail.com>
2022-11-29 12:33:48 -0300

    Update User

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,10 @@ import { Router, Request, Response } from 'express';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
+import { UpdateUserController } from './controllers/user/UpdateUserController';
 import { isAuthenticated } from './middlewares/isAuthenticated';
+
+
 
 
 const router = Router()
@@ -12,6 +15,8 @@ const router = Router()
 router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
 
-router.get('/me', isAuthenticated, new DetailUserController().handle)
+router.get('/me', isAuthenticated, new DetailUserController().handle);
+
+router.put('/users', isAuthenticated, new UpdateUserController().handle);
 
 export { router }
