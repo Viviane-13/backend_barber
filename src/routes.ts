4a7e37170bf09cd16ac5b0df994640087e0c1c11
Viviane Oliveira <oviviane772@gmail.com>
2022-11-29 15:20:01 -0300

    create haircut

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,10 @@
 import { Router, Request, Response } from 'express';
+import { CreateHaircutController } from './controllers/haircut/CreateHaircutController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { UpdateUserController } from './controllers/user/UpdateUserController';
 import { isAuthenticated } from './middlewares/isAuthenticated';
-
-
 
 
 const router = Router()
@@ -14,9 +13,13 @@ const router = Router()
 
 router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
-
 router.get('/me', isAuthenticated, new DetailUserController().handle);
-
 router.put('/users', isAuthenticated, new UpdateUserController().handle);
+
+//--- ROTAS HAIRCUTS ---
+
+router.post('/haircut', isAuthenticated, new CreateHaircutController().handle)
+
+
 
 export { router }
